48aa0cd9bcc6e66b1a53f898b7219844fd308605
TheShaman29 <76856664+TheShaman29@users.noreply.github.com>
2021-01-02 03:30:59 +0100

    Update beh.js

--- js/beh.js
+++ js/beh.js
@@ -22,20 +22,20 @@ showMiliseconds()*/
 
 function resize(){
     document.querySelector('#place4ad').style.height = (2*window.innerHeight/3+15)+"px"
-    document.querySelector('#hello').style.height = window.innerHeight+"px"
+    document.querySelector('#hello').style.minHeight = window.innerHeight+"px"
 
     document.querySelector('#hello>div').style.top = ((document.querySelector('#hello').offsetHeight-document.querySelector('#hello>div').offsetHeight-50)/2)+"px"
 
 
 
-    document.querySelector('#games').style.height = window.innerHeight+"px"
+    document.querySelector('#games').style.minHeight = window.innerHeight+"px"
 
     document.querySelector('#games>div').style.top = ((document.querySelector('#games').offsetHeight-document.querySelector('#games>div').offsetHeight-50)/2)+"px"
     /*if(document.querySelector('#articles').offsetHeight<window.innerHeight){
         document.querySelector('#articles').style.height = window.innerHeight+"px"
     }*/
 
-    document.querySelector('#articles-banner').style.height = window.innerHeight+"px"
+    document.querySelector('#articles-banner').style.minHeight = window.innerHeight+"px"
     document.querySelector('#articles-banner > div').style.top = (window.innerHeight/6-25)+"px"
     
     for(let e of [...document.querySelectorAll('#articles article')/*,document.querySelector('#contact'),document.querySelector('footer')*/]){
